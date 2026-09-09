44ddbf5cf2fdedc42ff84b496a898f79100b780a
ahmedebrahem0 <amezo2866@gmail.com>
2026-09-09 12:10:01 +0300

    Feat: Handel Student Attendance Route

--- src/app/(dashboard)/student/my-attendance/page.tsx
+++ src/app/(dashboard)/student/my-attendance/page.tsx
@@ -1,4 +1,10 @@
+import { MyAttendancePage } from "@/features/students/components/MyAttendancePage"
+
+export const metadata = {
+  title: "My Attendance | EduSystem",
+}
+
 export default function Page() {
-  return null;
+  return <MyAttendancePage />
 }
 
