a86c3a39fcb62a327e2181c4f0255c446f0107f1
ahmedebrahem0 <amezo2866@gmail.com>
2026-09-09 12:09:54 +0300

    Feat: Handel Student My Profile Route

--- src/app/(dashboard)/student/my-profile/page.tsx
+++ src/app/(dashboard)/student/my-profile/page.tsx
@@ -1,4 +1,10 @@
+import { MyProfilePage } from "@/features/students/components/MyProfilePage"
+
+export const metadata = {
+  title: "My Profile | EduSystem",
+}
+
 export default function Page() {
-  return null;
+  return <MyProfilePage />
 }
 
