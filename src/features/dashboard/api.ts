5ce2400e9e6489050e03b89305f0a17e2141f5a5
ahmedebrahem0 <amezo2866@gmail.com>
2026-09-10 10:10:16 +0300

    Feat: Handel Dashboard API Exports

---
+++ src/features/dashboard/api.ts
@@ -0,0 +1,8 @@
+export { useGetAdminUsersQuery } from "@/features/admin/api";
+export { useGetAttendancesQuery } from "@/features/attendances/api";
+export { useGetClassesQuery } from "@/features/classes/api";
+export { useGetGradesQuery, useGetMyGradesQuery } from "@/features/grades/api";
+export { useGetStudentAttendanceQuery, useGetStudentsQuery } from "@/features/students/api";
+export { useGetSubjectsQuery } from "@/features/subjects/api";
+export { useGetTeachersQuery } from "@/features/teachers/api";
+export { useGetMyTimetableQuery } from "@/features/timetables/api";
