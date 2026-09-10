44ba364092af8b083b23e54316e21cc94ca0c07c
ahmedebrahem0 <amezo2866@gmail.com>
2026-09-10 10:21:54 +0300

    Feat: Handel Teacher Subject Names Type

--- src/features/teacherSubjects/types.ts
+++ src/features/teacherSubjects/types.ts
@@ -1,6 +1,8 @@
 export interface TeacherSubject {
   teacherId: number;
+  teacherName?: string | null;
   subjectId: number;
+  subjectName?: string | null;
 }
 
 export interface CreateTeacherSubjectDto {
